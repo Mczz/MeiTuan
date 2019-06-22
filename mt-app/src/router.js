3df84b3e2d6d1fb2569a62d13a68e7efb828479a
Mc <469090292@qq.com>
2019-06-22 22:25:58 +0800

    default created

--- mt-app/src/router.js
+++ mt-app/src/router.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
-
+import defaultPage from '@/base/default.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -10,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home
+      name: 'default',
+      component: defaultPage
     },
     {
       path: '/about',
@@ -19,7 +18,7 @@ export default new Router({
       // route level code-splitting
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      // component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     }
   ]
 })
